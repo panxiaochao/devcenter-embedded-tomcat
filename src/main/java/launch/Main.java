4e75abbe78ec50476eb678bf288751d1a3ae88d3
panxiaochao <panxiaochao602@qq.com>
2015-07-27 15:30:05 +0800

    change

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -10,7 +10,6 @@ public class Main {
         Tomcat tomcat = new Tomcat();
 
         //The port that we should run on can be set into an environment variable
-        //Look for that variable and default to 8080 if it isn't there.
         String webPort = System.getenv("PORT");
         if(webPort == null || webPort.isEmpty()) {
             webPort = "8080";
